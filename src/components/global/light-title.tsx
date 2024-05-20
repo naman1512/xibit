c30c5aae86c92794a3b7d39c5dafdc813e99fd85
naman1512 <141447023+naman1512@users.noreply.github.com>
2024-05-20 13:45:28 +0530

    Update light-title.tsx

--- src/components/global/light-title.tsx
+++ src/components/global/light-title.tsx
@@ -71,7 +71,7 @@ export function SpotlightPreview() {
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          PRODCUTIVITY FOR EVERYBODY <br /> AUTOMATION FOR EVERYONE
+          PRODUCTIVITY FOR EVERYBODY <br /> AUTOMATION FOR EVERYONE
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
         Automation is a powerful tool for enhancing productivity and streamlining task management. With Xibit, you can seamlessly integrate your workflows with popular platforms such as Notion, Slack, Google Drive, and Discord. Effortlessly connect your workplaces and unleash the full potential of automation to boost efficiency and simplify your work life.
